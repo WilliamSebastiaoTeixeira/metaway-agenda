23be1b82b380ffd79fae3919e99a10ef1cc859c4
William Sebastião Teixeira <williamsebastiaoteixeira@gmail.com>
2024-05-11 00:48:43 -0300

    fix logout button

--- src/components/web-layout/Drawer.vue
+++ src/components/web-layout/Drawer.vue
@@ -9,28 +9,53 @@
     @mouseover="onMouseMinistate(false)"
     @mouseout="onMouseMinistate(true)"
   >
-    <q-scroll-area class="fit">
-      <DrawerList :mini-state="miniState" />
-    </q-scroll-area>
+    <div class="column no-wrap justify-around" style="height: 100%">
+      <q-scroll-area class="column justify-between fit">
+        <DrawerList :mini-state="miniState" />
+      </q-scroll-area>
+
+      <q-list padding>
+        <q-item clickable class="text-white" @click="onLogout">
+          <q-item-section avatar class="">
+            <q-icon size="20px" name="las la-sign-out-alt" />
+          </q-item-section>
+
+          <q-item-section>
+            <span size="20px"> Sair </span>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </div>
   </q-drawer>
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref, watch } from 'vue'
 import { storeToRefs } from 'pinia'
+import { useRouter } from 'vue-router'
 
 import { useGeneralStore } from 'src/stores/general'
+import { useAuthorizationStore } from 'src/stores/authorization'
 
 import DrawerList from './DrawerList.vue'
 
+const router = useRouter()
+
 const generalStore = useGeneralStore()
 const { general } = storeToRefs(generalStore)
+
+const auth = useAuthorizationStore()
 
 const miniState = ref(false)
 
 function onMouseMinistate(event: boolean) {
   if (!general.value.flexibleDrawer) return
   miniState.value = event
+}
+
+async function onLogout() {
+  auth.logout()
+  router.push('/')
 }
 
 watch(
--- src/components/web-layout/MenuToolBar.vue
+++ src/components/web-layout/MenuToolBar.vue
@@ -37,33 +37,13 @@
         </div>
       </q-item-section>
     </q-item>
-
-    <q-item clickable @click="onLogout">
-      <q-item-section avatar>
-        <q-avatar>
-          <q-icon color="grey-6" name="las la-sign-out-alt" />
-        </q-avatar>
-      </q-item-section>
-      <q-item-section>
-        <span class="text-grey-6 text-bold"> Sair </span>
-      </q-item-section>
-    </q-item>
   </q-menu>
 </template>
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
 import { storeToRefs } from 'pinia'
 
 import { useGeneralStore } from 'src/stores/general'
-import { useAuthorizationStore } from 'src/stores/authorization'
 
-const router = useRouter()
-const auth = useAuthorizationStore()
 const generalStore = useGeneralStore()
 const { general } = storeToRefs(generalStore)
-
-async function onLogout() {
-  auth.logout()
-  router.push('/')
-}
 </script>
