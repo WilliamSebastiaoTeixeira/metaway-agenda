c867da3af313aa4630c6c62d9e68826efbfe9e3a
William Sebastião Teixeira <williamsebastiaoteixeira@gmail.com>
2024-05-11 21:22:19 -0300

    types Favorito Request and Response

--- src/api/favorito.ts
+++ src/api/favorito.ts
@@ -1,22 +1,40 @@
 import { api } from 'src/boot/axios'
+
+import type { Contato } from 'src/types/contato'
+import type { ResponseWrapper } from 'src/types/responseWrapper'
+
+export type FavoritoPesquisarResponse = Contato[]
+
+export type FavoritoRemoverResponse = ResponseWrapper<boolean>
+
+export type FavoritoSalvarRequest = Contato
+
+export type FavoritoSalvarResponse = ResponseWrapper<boolean>
+
 export default class FavoritoApi {
   pesquisar = {
     get: async function () {
-      const { data } = await api.get('favorito/pesquisar')
+      const { data } =
+        await api.get<FavoritoPesquisarResponse>('favorito/pesquisar')
       return data
     },
   }
 
   remover = {
     delete: async function (contatoId: string) {
-      const { data } = await api.delete(`/api/favorito/remover/${contatoId}`)
+      const { data } = await api.delete<FavoritoRemoverResponse>(
+        `/api/favorito/remover/${contatoId}`,
+      )
       return data
     },
   }
 
   salvar = {
-    post: async function (params: any) {
-      const { data } = await api.post('favorito/salvar', params)
+    post: async function (params: FavoritoSalvarRequest) {
+      const { data } = await api.post<FavoritoSalvarResponse>(
+        'favorito/salvar',
+        params,
+      )
       return data
     },
   }
