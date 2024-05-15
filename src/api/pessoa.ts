71a157f6e89877ff659d6110fe190822a20f1ed0
William Sebastião Teixeira <williamsebastiaoteixeira@gmail.com>
2024-05-15 19:34:21 -0300

    pessoa type

--- src/api/pessoa.ts
+++ src/api/pessoa.ts
@@ -1,29 +1,57 @@
 import { api } from 'src/boot/axios'
+import type { Pessoa } from 'src/types/pessoa'
+
+import type { ResponseWrapper } from 'src/types/responseWrapper'
+
+export type PessoaBuscarResponse = ResponseWrapper<Pessoa>
+
+export type PessoaPesquisarResquest = {
+  nome: string
+}
+
+export type PessoaPesquisarResponse = Pessoa[]
+
+export type PessoaRemoverResponse = ResponseWrapper<boolean>
+
+export type PessoaSalvarRequest = Pessoa
+
+export type PessoaSalvarResponse = ResponseWrapper<Pessoa>
+
 export default class PessoaApi {
   buscar = {
     get: async function (id: number) {
-      const { data } = await api.get(`/api/pessoa/buscar/${id}`)
+      const { data } = await api.get<PessoaBuscarResponse>(
+        `/api/pessoa/buscar/${id}`,
+      )
       return data
     },
   }
 
   pesquisar = {
-    post: async function (params: any) {
-      const { data } = await api.post('pessoa/pesquisar', params)
+    post: async function (params: PessoaPesquisarResquest) {
+      const { data } = await api.post<PessoaPesquisarResponse>(
+        'pessoa/pesquisar',
+        params,
+      )
       return data
     },
   }
 
   remover = {
     delete: async function (id: number) {
-      const { data } = await api.delete(`pessoa/remover/${id}`)
+      const { data } = await api.delete<PessoaRemoverResponse>(
+        `pessoa/remover/${id}`,
+      )
       return data
     },
   }
 
   salvar = {
-    post: async function (params: any) {
-      const { data } = await api.post('pessoa/salvar', params)
+    post: async function (params: PessoaSalvarRequest) {
+      const { data } = await api.post<PessoaSalvarResponse>(
+        'pessoa/salvar',
+        params,
+      )
       return data
     },
   }
