5ab00364f126668a937be96d5f2120b3e130249f
William Sebastião Teixeira <williamsebastiaoteixeira@gmail.com>
2024-05-15 19:48:04 -0300

    Usuario types

--- src/api/usuario.ts
+++ src/api/usuario.ts
@@ -4,22 +4,56 @@ import { RoleUsuarioEnum } from 'src/types/enum/RoleUsuario'
 import type { ResponseWrapper } from 'src/types/responseWrapper'
 import type { Usuario } from 'src/types/usuario'
 
+export type UsuarioAlterarSenhaRequest = {
+  newPassword: string
+  password: string
+  username: string
+}
+
+export type UsuarioAlterarSenhaResponse = ResponseWrapper<Usuario>
+
+export type UsuarioAtualizarRequest = Usuario
+
+export type UsuarioAtualizarResponse = ResponseWrapper<Usuario>
+
 export type UsuariosBuscarResponse = ResponseWrapper<{
   usuario: Usuario
   tipos: RoleUsuarioEnum[]
 }>
 
+export type UsuarioPesquisarRequest = {
+  termo: string
+}
+
+export type UsuarioPesquisarResponse = Usuario[]
+
+export type UsuarioSalvarResquest = {
+  tipos: RoleUsuarioEnum[]
+  usuario: Usuario
+}
+
+export type UsuarioSalvarResponse = ResponseWrapper<{
+  tipos: RoleUsuarioEnum[]
+  usuario: Usuario
+}>
+
 export default class UsuarioApi {
   alterarSenha = {
-    post: async function (params: any) {
-      const { data } = await api.post('usuario/alterarSenha', params)
+    post: async function (params: UsuarioAlterarSenhaRequest) {
+      const { data } = await api.post<UsuarioAlterarSenhaResponse>(
+        'usuario/alterarSenha',
+        params,
+      )
       return data
     },
   }
 
   atualizar = {
-    put: async function (params: any) {
-      const { data } = await api.put('usuario/atualizar', params)
+    put: async function (params: UsuarioAtualizarRequest) {
+      const { data } = await api.put<UsuarioAtualizarResponse>(
+        'usuario/atualizar',
+        params,
+      )
       return data
     },
   }
@@ -34,15 +68,21 @@ export default class UsuarioApi {
   }
 
   pesquisar = {
-    post: async function (params: any) {
-      const { data } = await api.post('usuario/pesquisar', params)
+    post: async function (params: UsuarioPesquisarRequest) {
+      const { data } = await api.post<UsuarioPesquisarResponse>(
+        'usuario/pesquisar',
+        params,
+      )
       return data
     },
   }
 
   salvar = {
-    post: async function (params: any) {
-      const { data } = await api.post('usuario/salvar', params)
+    post: async function (params: UsuarioSalvarResquest) {
+      const { data } = await api.post<UsuarioSalvarResponse>(
+        'usuario/salvar',
+        params,
+      )
       return data
     },
   }
