7d8a30329f7bd3357b0509907fc4172044e08537
WilliamSebastiaoTeixeira <williamsebastiaoteixeira@gmail.com>
2024-05-16 22:42:01 +0000

    Deploying to gh-pages from @ WilliamSebastiaoTeixeira/metaway-agenda@fc78801eefc03a221316d369bbd120abee288468 🚀

---
+++ assets/Pessoas.579a75ce.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./plugin-vue_export-helper.21dcd24c.js";import{o,aB as r}from"./index.43b153ea.js";const s={};function a(c,t){return o(),r("div",null,"Pessoas")}var f=e(s,[["render",a]]);export{f as default};
