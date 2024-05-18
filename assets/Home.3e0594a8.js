f4538569ca448b9dd7c83c002ad5a147c9508522
WilliamSebastiaoTeixeira <williamsebastiaoteixeira@gmail.com>
2024-05-18 16:29:46 +0000

    Deploying to gh-pages from @ WilliamSebastiaoTeixeira/metaway-agenda@f71d757cd6262c07e2ec9d88a59aeedbd11607fe 🚀

---
+++ assets/Home.3e0594a8.js
@@ -0,0 +1,1 @@
+import{Q as e}from"./QPage.f8337f99.js";import{_ as r}from"./plugin-vue_export-helper.21dcd24c.js";import{o as t,i as o,w as a,a_ as c}from"./index.dc740a0a.js";const s={};function _(f,m){return t(),o(e,{class:"row col justify-center items-center"},{default:a(()=>[c(" Home ")]),_:1})}var p=r(s,[["render",_]]);export{p as default};
